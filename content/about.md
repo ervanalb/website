f0e892f5a65e0c7e5c7fefce92358e3bed33d850
Eric Van Albert <eric@van.al>
2016-06-17 19:07:30 -0400

    add photos to about page

--- content/about.md
+++ content/about.md
@@ -8,5 +8,11 @@ I graduated MIT in 2014. I lived in [EC](http://eastcamp.us/) ([Tetazoo](http://
 
 # Things I like to do
 
-* Picking locks
-* Skydiving
+## Picking locks
+<p class="images">
+[![](/media/about/lockpicking_thumb.jpg)](/media/about/lockpicking.jpg "")
+</p>
+## Skydiving
+<p class="images">
+[![](/media/about/skydiving_thumb.jpg)](/media/about/skydiving.jpg "")
+</p>
--- generate_album.py
+++ generate_album.py
@@ -16,9 +16,9 @@ def process(fn):
     thumb_fn = full_fn
     if thumb_fn.endswith(".jpg"):
         thumb_fn = thumb_fn[:-4] + "_thumb.jpg"
-    return (full_fn, thumb_fn, str(tags["EXIF DateTimeOriginal"]))
+    return (full_fn, thumb_fn, str(tags.get("EXIF DateTimeOriginal","")))
 
-photos = [process(fn) for fn in filenames]
+photos = [process(fn) for fn in filenames if not fn.endswith("_thumb.jpg")]
 photos.sort(key=lambda x: x[2])
 
 for (web_fn, thumb_fn, _) in photos:
